5cf8508cac22e7e1cfd30a82ce88f572c61c512c
thgthg1 <941691647@qq.com>
2022-08-07 07:22:50 -0700

    test

--- gitup.sh
+++ gitup.sh
@@ -1,3 +1,11 @@
-git add *;
-git commit -m "test";
-git push;
+#!/bin/bash
+read option;
+echo $option;
+if option=1; then
+ echo "git upload";
+ git add .;
+ git commit -m "test";
+ git push;
+else
+ echo "git 0";
+fi
